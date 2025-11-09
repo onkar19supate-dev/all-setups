92daed87af9d8187c1d60ba39ea8d8007d42b3d2
Onkar <onkar19supate@gmail.com>
2025-11-09 10:34:57 +0530

    Update ubuntu-tomcat.sh

--- ubuntu-tomcat.sh
+++ ubuntu-tomcat.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 sudo apt update
 apt install openjdk-17-jre-headless -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.110/bin/apache-tomcat-9.0.110.tar.gz
-tar -zxvf apache-tomcat-9.0.110.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.110/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.110/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="onkar123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.110/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.110/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.110/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.110/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.110/webapps/manager/META-INF/context.xml
-sudo sh apache-tomcat-9.0.110/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.111/bin/apache-tomcat-9.0.111.tar.gz
+tar -zxvf apache-tomcat-9.0.111.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="onkar123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.111/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.111/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.111/webapps/manager/META-INF/context.xml
+sudo sh apache-tomcat-9.0.111/bin/startup.sh
 
